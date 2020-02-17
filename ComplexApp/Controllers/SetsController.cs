446a9795bb08e186997cb651bd6624c72d7b701c
Jonathan Valencia <alexisvalenciav@hotmail.com>
2020-02-16 23:34:41 -0500

    Agregar archivos de proyecto.

---
+++ ComplexApp/Controllers/SetsController.cs
@@ -0,0 +1,14 @@
+﻿using ComplexApp.Models;
+using System.Web.Mvc;
+
+namespace ComplexApp.Controllers
+{
+    public class SetsController : Controller
+    {
+        Sets aSet = new Sets();
+        public ActionResult Sets()
+        {
+            return View(aSet);
+        }
+    }
+}
---
+++ ComplexApp/Models/Sets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComplexApp.Models
+{
+    public class Sets
+    {
+        public Sets()
+        {
+            this.Elements = new HashSet<string>();
+            this.Elements.Add("Elemento 1");
+            this.Elements.Add("Elemento 2");
+            this.Elements.Add("Elemento 3");
+        }
+
+        public int Id { get; set; }
+        public HashSet<string> Elements{ get; set; }
+
+    }
+}
