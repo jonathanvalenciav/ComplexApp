fbe322646ecfdfcbc76f8e7fbe939e202f14bb7c
Jonathan Valencia <alexisvalenciav@hotmail.com>
2020-03-05 01:32:39 -0500

    -Se agrega funcionalidad para validar la existencia de los elementos que se estàn intentando ingresar al conjunto.
    -Se valida con una expresión regular el formato para ingresar las cadenas de texto que contienen varios elementos para el conjunto.
    
    -Se muestran mensajes de instrucción para el usuario desde la vista.

--- ComplexApp/Services/SetsService.cs
+++ ComplexApp/Services/SetsService.cs
@@ -1,23 +1,50 @@
 ﻿using ComplexApp.Models;
-using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ComplexApp.Services
 {
     public class SetsService
     {
-        Sets globalSet = (Sets)System.Web.HttpContext.Current.Session["Sets"];
-        public void addElement(String element)
-        {
-            int a = globalSet.Elements.Count;
-            globalSet.Elements.Add(element);
-            int b = globalSet.Elements.Count;
+        public const char ELEMENTS_SEPARATOR = ',';
+        public const string EMPTY_STRING = "";
 
+        Sets globalSet = (Sets)System.Web.HttpContext.Current.Session["Sets"];
+        string patternMultipleElements = @"^[\w\s._]+(?:\,[\w\s._]+)+$";
+
+
+        public void addElement(string stringElement) {
+            if (existsMultiplesElements(stringElement))
+            {
+                addMultiplesElements(stringElement);
+            } else {
+                addSingleElement(stringElement);
+            }
+        }
+
+        private bool existsMultiplesElements(string elementToAdd) {
+            return Regex.IsMatch(elementToAdd, patternMultipleElements);        
+        }
+
+        private void addSingleElement(string element)
+        {
+            if (!globalSet.Elements.Contains(element) && !element.Equals(EMPTY_STRING)){
+                globalSet.Elements.Add(element);
+            }            
+        }
+
+        private void addMultiplesElements(string StringOfElements)
+        {
+            string[] elements = StringOfElements.Split(ELEMENTS_SEPARATOR).Distinct().ToArray();
+            
+            foreach (string aElement in elements) {
+                addSingleElement(aElement);
+            }
         }
 
         public void removeElementFromSet(string element)
         {
             globalSet.Elements.Remove(element);
         }
-
     }
 }
