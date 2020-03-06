9a152ad37022e63e78dd4d64f8e92e3b6c72b19e
Juan Camilo Gil Cordoba <juan.gil9768@uco.net.co>
2020-03-05 20:12:26 -0500

    Terminado

--- ComplexApp/Services/SetsService.cs
+++ ComplexApp/Services/SetsService.cs
@@ -8,10 +8,13 @@ namespace ComplexApp.Services
     {
         public const char ELEMENTS_SEPARATOR = ',';
         public const string EMPTY_STRING = "";
+        public const char SPACE = ' ';
+        public const char SYMBOL_EMPTY = '∅';
 
         Sets globalSet = (Sets)System.Web.HttpContext.Current.Session["Sets"];
         string patternMultipleElements = @"^[\w\s._]+(?:\,[\w\s._]+)+$";
-
+        string patternSingleElement = @"^[\w\s\d.]+$";
+        string patterEmptyElement = @"^[\s] +$";
 
         public void addElement(string stringElement) {
             if (existsMultiplesElements(stringElement))
@@ -26,10 +29,22 @@ namespace ComplexApp.Services
             return Regex.IsMatch(elementToAdd, patternMultipleElements);        
         }
 
+        private bool isValidElement(string elementToAdd)        {
+            return Regex.IsMatch(elementToAdd, patternSingleElement);
+        }
+        private bool isEmptyElement(string elementToAdd)
+        {
+            return Regex.IsMatch(elementToAdd, patterEmptyElement);
+        }
+
         private void addSingleElement(string element)
         {
-            if (!globalSet.Elements.Contains(element) && !element.Equals(EMPTY_STRING)){
-                globalSet.Elements.Add(element);
+            if (isValidElement(element) && !globalSet.Elements.Contains(element) && !element.Equals(EMPTY_STRING)){
+                if (isEmptyElement(element))
+                {
+                    element = SYMBOL_EMPTY.ToString();
+                }
+                globalSet.Elements.Add(element.Trim(SPACE));
             }            
         }
 
